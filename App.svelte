d890282c1ef1055ff56842e55f59bb0892bdeb52
ajcwebdev <anthony@stepzen.com>
2021-03-08 22:34:29 -0800

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,12 @@
+<div class="App">
+  <header class="App-header">
+    <a
+      class="App-link"
+      href="https://svelte.dev"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn Svelte
+    </a>
+  </header>
+</div>
